e54d0deddd02e6cc02d638a82584ced2c69000b7
Rafael Schmitt <rschmitt5@slb.com>
2020-05-02 19:47:50 -0300

    REF - Remove random item generation

--- SpellChecker/Item.swift
+++ SpellChecker/Item.swift
@@ -13,28 +13,6 @@ class Item: Equatable {
         self.description = description
     }
 
-    convenience init(random: Bool = false) {
-        if random {
-            let adjectives = ["Fluffy", "Rusty", "Shiny"]
-            let nouns = ["Bear", "Spork", "Mac"]
-
-            var index = Int.random(in: 0 ..< adjectives.count)
-            let randomAdjective = adjectives[index]
-
-            index = Int.random(in: 0 ..< nouns.count)
-            let randomNoun = nouns[index]
-
-            let randomName = "\(randomAdjective) \(randomNoun)"
-            // let randomValue = Int.random(in: 0..<100)
-            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
-
-            self.init(name: randomName,
-                      description: randomSerialNumber)
-        } else {
-            self.init(name: "", description: "")
-        }
-    }
-
     static func == (lhs: Item, rhs: Item) -> Bool {
         return lhs.name == rhs.name
             && lhs.description == rhs.description
--- SpellChecker/ItemStore.swift
+++ SpellChecker/ItemStore.swift
@@ -22,14 +22,6 @@ class ItemStore {
         allItems.insert(movedItem, at: toIndex)
     }
 
-    @discardableResult func createItem() -> Item {
-        let newItem = Item(random: true)
-
-        allItems.append(newItem)
-
-        return newItem
-    }
-
     func removeItem(_ item: Item) {
         if let index = allItems.index(of: item) {
             allItems.remove(at: index)
