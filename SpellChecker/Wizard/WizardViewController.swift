11124173cb0a04352a075986200a001d13b7babd
Rafael Schmitt <rschmitt5@slb.com>
2020-05-03 20:51:34 -0300

    FEAT - Add more properties to Spell type, add SpellType, start Wizard screen

---
+++ SpellChecker/Wizard/WizardViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WizardViewController.swift
+//  SpellChecker
+//
+//  Created by Rafael Schmitt on 03/05/20.
+//  Copyright © 2020 Rafael Claycon Schmitt. All rights reserved.
+//
+
+import Foundation
