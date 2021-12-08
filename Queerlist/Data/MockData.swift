ad5d9386cb6f349c3b06023ee8ecad02a1b2ac3f
Rafael Schmitt <rafael.claycon@gmail.com>
2021-12-08 16:03:17 -0300

    Refactor images

--- Queerlist/Data/MockData.swift
+++ Queerlist/Data/MockData.swift
@@ -7,81 +7,84 @@ class MockData {
                                type: .gay,
                                famousQuote: nil,
                                description: "Alan Turing, born June 23, 1912 in London, England, was a British mathematician and logician who made major contributions to mathematics, cryptanalysis, logic, philosophy, and mathematical biology and also to the new areas later named computer science, cognitive science, artificial intelligence, and artificial life. In March 1952 he was convicted of “gross indecency”—that is to say, homosexuality, a crime in Britain at that time—and he was sentenced to 12 months of hormone “therapy.” Now with a criminal record, he would never again be able to work for Government Communications Headquarters (GCHQ), the British government’s postwar code-breaking centre. Alan was discovered dead in his bed, poisoned by cyanide. The official verdict was suicide, but no motive was established at the 1954 inquest.",
-                               image: UIImage(named: "Accio")!,
+                               image: UIImage(named: "alan")!,
                                date: Date())
         
         let cassia = Personality(name: "Cássia Eller",
                                  type: .lesbian,
                                  famousQuote: "I walk around, wanting to meet you on every corner I stop at every look, I leave the sadness and I bring hope in its place.",
                                  description: "Lorem ipsum",
-                                 image: UIImage(named: "Aguamenti"),
+                                 image: UIImage(named: "cassia"),
                                  date: Date())
             
         let laerte = Personality(name: "Laerte",
                                  type: .transgender,
                                  famousQuote: "People must be able to live the identity that seems most appropriate to them.",
                                  description: "Unlocks doors and other objects.",
-                                 image: nil,
+                                 image: UIImage(named: "laerte"),
                                  date: Date())
             
         let ludmilla = Personality(name: "Ludmilla",
                                    type: .bisexual,
                                    famousQuote: nil,
                                    description: "Clears the target's airway if they are choking on something.",
-                                   image: nil,
+                                   image: UIImage(named: "ludmilla"),
                                    date: Date())
             
         let marsha = Personality(name: "Marsha P. Johnson",
                                  type: .transvestite,
                                  famousQuote: "Revealing Charm",
                                  description: "Reveals secret messages written in invisible ink, or any other hidden markings. Also works against Concealing charms.",
-                                 image: nil,
+                                 image: UIImage(named: "marsha"),
                                  date: Date())
             
         let marta = Personality(name: "Marta",
                                 type: .lesbian,
                                 famousQuote: "Spider repelling spell",
                                 description: "Drives away spiders, including Acromantulas.",
-                                image: nil,
+                                image: UIImage(named: "marta"),
                                 date: Date())
             
         let missBia = Personality(name: "Miss Biá",
                                   type: .gay,
                                   famousQuote: "Slowing Charm",
                                   description: "Decreases the velocity of a moving target.",
-                                  image: nil,
+                                  image: UIImage(named: "missBia"),
                                   date: Date())
             
         let natalie = Personality(name: "Natalie Wynn",
                                   type: .transgender,
-                                  description: "")
+                                  famousQuote: "Trans liberation now!",
+                                  description: "Decreases the velocity of a moving target.",
+                                  image: UIImage(named: "natalie"),
+                                  date: Date())
         
         let pabllo = Personality(name: "Pabllo Vittar",
                                  type: .gay,
                                  famousQuote: nil,
                                  description: "Transforms the target into a bird.",
-                                 image: nil,
+                                 image: UIImage(named: "pabllo"),
                                  date: Date())
         
         let paulo = Personality(name: "Paulo Iotti",
                                 type: .gay,
                                 famousQuote: nil,
                                 description: "Transforms the target into a bird.",
-                                image: nil,
+                                image: UIImage(named: "paulo"),
                                 date: Date())
             
         let rupaul = Personality(name: "RuPaul",
                                  type: .gay,
                                  famousQuote: nil,
                                  description: "Transforms the target into a bird.",
-                                 image: nil,
+                                 image: UIImage(named: "rupaul"),
                                  date: Date())
             
         let thammy = Personality(name: "Thammy Miranda",
                                  type: .transgender,
                                  famousQuote: nil,
                                  description: "Transforms the target into a bird.",
-                                 image: nil,
+                                 image: UIImage(named: "thammy"),
                                  date: Date())
         
         spellStore.allItems.append(alan)
