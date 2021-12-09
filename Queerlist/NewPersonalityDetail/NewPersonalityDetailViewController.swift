47a35f014ab6c91e6280a90e43a84c170d1ccda7
Rafael Schmitt <rafael.claycon@gmail.com>
2021-12-09 19:27:45 -0300

    Add NewPersonalityDetailViewController

--- Queerlist/NewPersonalityDetail/NewPersonalityDetailViewController.swift
+++ Queerlist/NewPersonalityDetail/NewPersonalityDetailViewController.swift
@@ -1,1 +1,41 @@
-import Foundation
+import UIKit
+
+internal class NewPersonalityDetailViewController: UIViewController {
+    
+    private lazy var baseView: NewPersonalityDetailView = {
+        let view = NewPersonalityDetailView()
+        return view
+    }()
+    
+//    internal init(presenter: OrderRefundIncidentOpeningPresenterProtocol) {
+//        self.presenter = presenter
+//        super.init()
+//        hidesBottomBarWhenPushed = true
+//    }
+    
+    @available(*, unavailable)
+    internal required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    internal override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        // presenter.moduleLoaded()
+    }
+    
+}
+
+extension NewPersonalityDetailViewController: CodableView {
+    
+    internal func buildViews() {
+        view.addSubview(baseView)
+    }
+    
+    internal func configConstraints() {
+        baseView.snp.makeConstraints { constraint in
+            constraint.edges.equalToSuperview()
+        }
+    }
+    
+}
